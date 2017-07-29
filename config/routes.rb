fb498598e8fdd957ee0d8ab590c1c3a7d03b7b9b
sixtoguerra1982 <sixto.guerra1982@gmail.com>
2017-07-29 14:59:09 -0400

    tercer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/three'
+
   get 'pages/one'
 
   get 'pages/two'
+
+  get 'pages/three'
 
   root 'pages#one'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
